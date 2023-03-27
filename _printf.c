fbe53741b145d8683f3c72597a716ef5ac570389
Sospeter <sosmogendi@gmail.com>
2023-03-27 12:18:11 -0700

    Print according to format

--- _printf.c
+++ _printf.c
@@ -1,49 +1,30 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+
 /**
- * _printf - prints output according to a format
- * @format: is a character string
- * @...: variable args
- * Return: Number of characters printed
- */
-
-
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
+ *
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-	int i = 0, count = 0, chars;
+	int size;
 	va_list args;
 
+	if (format == NULL)
+		return (-1);
+
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
+
 	va_start(args, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	while (format[i])
-	{
-		chars = 0;
-		if (format[i] == '%')
-		{
-			if (!format[i + 1] || (format[i + 1] == ' ' && !format[i + 2]))
-			{
-				count = -1;
-				break;
-			}
-			chars += get_function(format[i + 1], args);
-			if (chars == 0)
-				count += _putchar(format[i + 1]);
-			if (chars == -1)
-				count = -1;
-			i++;
-		}
-		else
-		{
-			(count == -1) ? (_putchar(format[i])) : (count += _putchar(format[i]));
-		}
-		i++;
-		if (count != -1)
-			count += chars;
-	}
+	size = handler(format, args);
+
+	_putchar(-1);
 	va_end(args);
-	return (count);
+
+	return (size);
 }
